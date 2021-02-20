5b0f1e8e14dbae838afc6558097dd734dfbd33bf
ceciDucos <58122952+ceciDucos@users.noreply.github.com>
2021-02-19 22:43:43 -0300

    first commit

---
+++ bombsaway/src/test/java/com/example/bombsaway/BombsawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bombsaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BombsawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
